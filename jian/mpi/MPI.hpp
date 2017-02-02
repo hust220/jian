501c054239f188598fd42813e4a162a29ecfba2c
wangjian <wj_hust08@hust.edu.cn>
2017-02-02 11:57:04 +0800

    aa

--- jian/mpi/MPI.hpp
+++ jian/mpi/MPI.hpp
@@ -9,48 +9,17 @@
 
 BEGIN_JN
 
-class MPI {
-public:
-	int m_rank;
-	int m_size;
+void mpi_init(int *argc, char ***argv);
 
-	MPI() {
-		MPI_Init(&g_argc, &g_argv);
-		m_rank = rank();
-		m_size = size();
-	}
+int mpi_rank();
 
-	int rank() {
-		int rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		return rank;
-	}
+int mpi_size();
 
-	int size() {
-		int size;
-		MPI_Comm_size(MPI_COMM_WORLD, &size);
-		return size;
-	}
+void mpi_send();
 
-	void send(S s, int rank) {
-		MPI_Send(s.c_str(), s.size(), MPI_CHAR, rank, 0, MPI_COMM_WORLD);
-	}
+Str mpi_recv();
 
-	S recv(int rank) {
-		char hi[1024];
-		MPI_Status status;
-		//int n = MPI_Recv(hi, 1024, MPI_CHAR, rank, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-		MPI_Recv(hi, 1024, MPI_CHAR, rank, 0, MPI_COMM_WORLD, &status);
-		int n = ((int*)&status)[0];
-		return S(hi, n);
-	}
-
-	~MPI() {
-		MPI_Finalize();
-	}
-};
-
-extern SP<MPI> g_mpi;
+void mpi_free();
 
 END_JN
 
