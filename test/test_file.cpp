559be668e0a32e9ec72ae94cfffd8cd43272962c
wangjian <wj_hust08@hust.edu.cn>
2017-02-02 09:59:09 +0800

    aa

--- test/test_file.cpp
+++ test/test_file.cpp
@@ -5,16 +5,20 @@ BEGIN_JN
 
 TEST_CASE(file_test)
 {
-	STD_ ofstream ofile("file-test.txt");
+    char filename[] = "jian-test-file.txt";
+
+	STD_ ofstream ofile(filename);
 	ofile << "hi" << STD_ endl;
 	ofile << "hello";
 	ofile.close();
 
     List<Str> ls;
-	for (auto &&it : JN_ FileLines("file-test.txt")) {
+	for (auto &&it : JN_ FileLines(filename)) {
         ls.push_back(it.line);
 	}
     TEST_CHECK(size(ls) == 2);
+
+    std::remove(filename);
 }
 
 END_JN
